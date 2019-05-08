836ffaf48832c4fecc11b6f70ea72ebc6b1f85d8
ieoal279dieu <50416713+ieoal279dieu@users.noreply.github.com>
2019-05-09 00:21:51 +0700

    Add files via upload
    add

---
+++ ModalBox/1.js
@@ -0,0 +1,23 @@
+﻿ $(function(){
+ 	
+ 	$('.noidungthe').addClass('noidungtheshow');
+ 	$('.gradiant').addClass('gradiantshow');
+
+ 	var thongbao = setInterval(function(){
+ 		$('.noidungthe').removeClass('noidungtheshow');
+ 		$('.gradiant').removeClass('gradiantshow');
+ 		clearInterval(thongbao);
+ 	},4000);
+
+ 	$('.buttonClick').click(function(event){
+ 		$('.noidungthe').addClass('noidungtheshow');
+ 		$('.gradiant').addClass('gradiantshow');
+
+ 	});
+ 	$('.buttonclose, .gradiant').click(function(event){
+ 		$('.noidungthe').removeClass('noidungtheshow');
+ 		$('.gradiant').removeClass('gradiantshow');
+ 	});
+
+})  
+ 
